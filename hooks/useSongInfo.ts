cdf3af0b8811ea1b65216bb0ddc67fe97c4313d2
Amritpreet Singh <amritpreet98@gmail.com>
2021-12-08 20:02:07 -0600

    feat(useSongInfo): defined interface

--- hooks/useSongInfo.ts
+++ hooks/useSongInfo.ts
@@ -3,11 +3,20 @@ import { useRecoilState } from 'recoil';
 import { currentTrackIdState } from '../atoms/songAtom';
 import { useSpotify } from './useSpotify';
 
+export interface Song {
+  album_type: string;
+  album: {
+    images: Array<{ height: number; url: string }>;
+  };
+  artists: Array<{ name: string }>;
+  name: string;
+}
+
 export const useSongInfo = () => {
   const spotifyService = useSpotify();
   const [currentTrackId, setCurrentTrackId] =
     useRecoilState(currentTrackIdState);
-  const [songInfo, setSongInfo] = useState(null);
+  const [songInfo, setSongInfo] = useState<Song | null>(null);
 
   useEffect(() => {
     const fetchSongInfo = async () => {
